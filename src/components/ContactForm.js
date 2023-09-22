650bf730a3402f321ac9d4e549011cda23471212
noaysk <nyasukagawa@myseneca.ca>
2023-09-21 22:04:10 -0400

    prepared Submission Complete

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react";
 
 class ContactForm extends React.Component {
+    
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitted: false,
+    };
+  }
+
   render() {
-    return (
-      <div className='contact-form'>
+    let contactForm;
+    if (this.state.isSubmitted) {
+      contactForm = <div className="contact-submit-message">Submission Complete</div>;
+    } else {
+      contactForm = (
         <form>
-          <p>Mail Address（required）</p>
-          {/* added input tag */}
-          <input />
-          
-          <p>Inquiry Form（required）</p>
-          {/* added textarea tag */}
-          <textarea />
-          
-          {/* added submit button */}
-          <input type='submit' value='Submit' />
-          
+        <p>Mail Address（required）</p>
+        <input />
+        <p>Inquiry Form（required）</p>
+        <textarea />
+        <input type='submit' value='Submit' />
         </form>
+      );
+    }
+
+
+    return (
+      <div className="contact-form">
+        {contactForm}
       </div>
     );
   }
