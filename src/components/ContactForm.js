9f4474840be93deafba388a78080bea28dd9f78a
noaysk <nyasukagawa@myseneca.ca>
2023-09-22 00:05:07 -0400

    added error message

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,52 +1,67 @@
 import React from "react";
 
 class ContactForm extends React.Component {
-    
   constructor(props) {
     super(props);
     this.state = {
       isSubmitted: false,
-      email: '',
+      email: "",
+      hasEmailError: false,
     };
   }
 
-  handleEmailChange(event){
+  handleEmailChange(event) {
     const inputValue = event.target.value;
-    this.setState({email: inputValue});
-    console.log(event.target.value)
+    const isEmpty = inputValue === "";
+    this.setState({ email: inputValue, hasEmailError: isEmpty });
   }
 
-  
-  handleSubmit(){
-    this.setState
-     ({isSubmitted: true});
-    
- }
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.state.email.length > 0) {
+      this.setState({ isSubmitted: true });
+      console.log("submit");
+    }else{
+        this.setState({ hasEmailError: true });
+        console.log("unsubmit");
+    }
+  };
 
   render() {
+    let emailErrorText;
+    if (this.state.hasEmailError) {
+      emailErrorText = (
+        <p className="contact-message-error">Email address is required</p>
+      );
+    }
+
     let contactForm;
     if (this.state.isSubmitted) {
-      contactForm = <div className="contact-submit-message">Submission Complete</div>;
+      contactForm = (
+        <div className="contact-submit-message">Submission Complete</div>
+      );
     } else {
       contactForm = (
-        <form onSubmit={()=>{this.handleSubmit()}}>
-        <p>Mail Address（required）</p>
-        <input value={this.state.email} 
-        onChange={(event)=>{this.handleEmailChange(event)}}       
-        />
-        <p>Inquiry Form（required）</p>
-        <textarea />
-        <input type='submit' value='Submit' />
+        <form onSubmit={this.handleSubmit}>
+          <p>Mail Address（required）</p>
+          <input
+            value={this.state.email}
+            onChange={(event) => {
+              this.handleEmailChange(event);
+            }}
+          />
+
+          {emailErrorText}
+
+          <p>Inquiry Form</p>
+          <textarea />
+          <input type="submit" value="Submit" />
         </form>
       );
     }
 
-
-    return (
-      <div className="contact-form">
-        {contactForm}
-      </div>
-    );
+    return <div className="contact-form">{contactForm}</div>;
   }
 }
 
